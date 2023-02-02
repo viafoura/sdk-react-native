da16000ef1382f5643cf81a5032e92a9b80fbd81
Martin De Simone <martindesimone481@gmail.com>
2023-02-02 15:55:05 -0500

    Updates

---
+++ android/app/src/main/java/com/sdk/RCTPreviewCommentsViewManager.java
@@ -0,0 +1,150 @@
+package com.sdk;
+
+import android.graphics.Color;
+import android.view.Choreographer;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.common.MapBuilder;
+import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.ViewGroupManager;
+import com.facebook.react.uimanager.annotations.ReactPropGroup;
+import com.viafourasdk.src.fragments.previewcomments.VFPreviewCommentsFragment;
+import com.viafourasdk.src.interfaces.VFLoginInterface;
+import com.viafourasdk.src.model.local.VFArticleMetadata;
+import com.viafourasdk.src.model.local.VFColors;
+import com.viafourasdk.src.model.local.VFSettings;
+import com.viafourasdk.src.model.local.VFSortType;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+
+public class RCTPreviewCommentsViewManager extends ViewGroupManager<FrameLayout> implements VFLoginInterface {
+
+    public static final String REACT_CLASS = "RNPreviewComments";
+    public final int COMMAND_CREATE = 1;
+    private int propWidth;
+    private int propHeight;
+
+    ReactApplicationContext reactContext;
+
+    public RCTPreviewCommentsViewManager(ReactApplicationContext reactContext) {
+        this.reactContext = reactContext;
+    }
+
+    @Override
+    public String getName() {
+        return REACT_CLASS;
+    }
+
+    /**
+     * Return a FrameLayout which will later hold the Fragment
+     */
+    @Override
+    public FrameLayout createViewInstance(ThemedReactContext reactContext) {
+        return new FrameLayout(reactContext);
+    }
+
+    /**
+     * Map the "create" command to an integer
+     */
+    @Nullable
+    @Override
+    public Map<String, Integer> getCommandsMap() {
+        return MapBuilder.of("create", COMMAND_CREATE);
+    }
+
+    /**
+     * Handle "create" command (called from JS) and call createFragment method
+     */
+    @Override
+    public void receiveCommand(
+            @NonNull FrameLayout root,
+            String commandId,
+            @Nullable ReadableArray args
+    ) {
+        super.receiveCommand(root, commandId, args);
+        int reactNativeViewId = args.getInt(0);
+        int commandIdInt = Integer.parseInt(commandId);
+
+        switch (commandIdInt) {
+            case COMMAND_CREATE:
+                createFragment(root, reactNativeViewId);
+                break;
+            default: {}
+        }
+    }
+
+    @ReactPropGroup(names = {"width", "height"}, customType = "Style")
+    public void setStyle(FrameLayout view, int index, Integer value) {
+        if (index == 0) {
+            propWidth = value;
+        }
+
+        if (index == 1) {
+            propHeight = value;
+        }
+    }
+
+    /**
+     * Replace your React Native view with a custom fragment
+     */
+    public void createFragment(FrameLayout root, int reactNativeViewId) {
+        ViewGroup parentView = (ViewGroup) root.findViewById(reactNativeViewId);
+        setupLayout(parentView);
+
+        try {
+            VFArticleMetadata articleMetadata = new VFArticleMetadata(new URL(""), "", "", new URL(""));
+            VFColors colors = new VFColors(ContextCompat.getColor(reactContext, R.color.colorVfDark), ContextCompat.getColor(reactContext, R.color.colorVf), Color.WHITE);
+            VFSettings settings = new VFSettings(colors);
+            FragmentActivity activity = (FragmentActivity) reactContext.getCurrentActivity();
+            final VFPreviewCommentsFragment myFragment = VFPreviewCommentsFragment.newInstance(activity.getApplication(), "", articleMetadata, this, settings, 10, VFSortType.mostLiked);
+            activity.getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(reactNativeViewId, myFragment, String.valueOf(reactNativeViewId))
+                    .commit();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void setupLayout(View view) {
+        Choreographer.getInstance().postFrameCallback(new Choreographer.FrameCallback() {
+            @Override
+            public void doFrame(long frameTimeNanos) {
+                manuallyLayoutChildren(view);
+                view.getViewTreeObserver().dispatchOnGlobalLayout();
+                Choreographer.getInstance().postFrameCallback(this);
+            }
+        });
+    }
+
+    /**
+     * Layout all children properly
+     */
+    public void manuallyLayoutChildren(View view) {
+        // propWidth and propHeight coming from react-native props
+        int width = propWidth;
+        int height = propHeight;
+
+        view.measure(
+                View.MeasureSpec.makeMeasureSpec(width, View.MeasureSpec.EXACTLY),
+                View.MeasureSpec.makeMeasureSpec(height, View.MeasureSpec.EXACTLY));
+
+        view.layout(0, 0, width, height);
+    }
+
+    @Override
+    public void startLogin() {
+
+    }
+}

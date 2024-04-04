9b451c0501bb0b74c278c2346a16b21ee1940308
Martin De Simone <mdesimone@itcrowdarg.com>
2024-04-04 20:38:43 -0300

    Updates

--- android/app/src/main/java/com/sdk/MainActivity.java
+++ android/app/src/main/java/com/sdk/MainActivity.java
@@ -1,5 +1,7 @@
 package com.sdk;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
@@ -7,10 +9,10 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
 public class MainActivity extends ReactActivity {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
   @Override
   protected String getMainComponentName() {
     return "sdk";

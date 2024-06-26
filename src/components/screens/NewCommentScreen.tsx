0dded556bdeba2a20223939f9e2f84bdd497ffbb
Martin De Simone <mdesimone@itcrowdarg.com>
2024-06-26 12:34:27 -0300

    Remove inline styles

--- src/components/screens/NewCommentScreen.tsx
+++ src/components/screens/NewCommentScreen.tsx
@@ -3,13 +3,18 @@ import React from 'react';
 import RNNewCommentAndroidComponent from '../../native/android/RNNewCommentAndroid.js';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { Screens } from '../../navigation/screens';
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: { height: '100%' },
+});
 
 const NewCommentScreen = () => {
   const route = useRoute();
   const navigation = useNavigation();
   return (
     <RNNewCommentAndroidComponent
-      style={{ height: '100%' }}
+      style={styles.container}
       containerId={route.params?.containerId}
       articleTitle={route.params?.articleTitle}
       articleSubtitle={route.params?.articleDesc}
--- src/components/screens/ProfileScreen.tsx
+++ src/components/screens/ProfileScreen.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import RNProfileAndroidComponent from '../../native/android/RNProfileAndroid.js';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { Screens } from '../../navigation/screens';
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: { height: '100%' },
+});
 
 const ProfileScreen = () => {
   const navigation = useNavigation();
@@ -9,7 +14,7 @@ const ProfileScreen = () => {
 
   return (
     <RNProfileAndroidComponent
-      style={{ height: '100%' }}
+      style={styles.container}
       userUUID={route.params?.userUUID}
       presentationType={route.params?.presentationType}
       darkMode={false}
